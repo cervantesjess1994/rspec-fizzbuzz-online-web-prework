d803b22c643b11be809e6db8848fe057ccccb8a2
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-06-15 08:12:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,8 @@
-require 'pry'
-
-def fizzbuzz
-  binding.pry
+def fizzbuzz(int)
+  if int % 3 == 0
+    puts "Fizz"
+  elsif int % 5 == 0
+    puts "Buzz"
+  else int % 15 == 0
+    puts "Fizzbuzz"
 end
